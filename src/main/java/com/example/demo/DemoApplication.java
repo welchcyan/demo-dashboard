58ec04d033022ef084b57adcd497dbd1303b1f55
chenwbj <chenwbj@gmail.com>
2017-10-20 20:31:19 +0800

    simplify

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,25 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @SpringBootApplication
-public class DemoApplication extends WebSecurityConfigurerAdapter{
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().and().csrf().ignoringAntMatchers("/admin/**");
-		http.antMatcher("/**")
-				.authorizeRequests()
-				.antMatchers("/index.html", "/login", "/", "/api/catalog/**",
-						"/user", "/assets/**").permitAll()
-				.anyRequest().authenticated().and().csrf().disable();
 	}
 }
